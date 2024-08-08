e1e8b2d4d687967a011cdc15b477b99f3e645381
Rain <xbmb250@hotmail.com>
2024-08-08 10:54:55 +0800

    作业

--- 99乘法表作业.py
+++ 99乘法表作业.py
@@ -1,4 +1,5 @@
-
-for n in range(1,10):
+s = int(input("数字"))
+s += 1
+for n in range(1,s):
     for j in range(1, n+1):
-        print(f'{n} x {j} = {n * j}\t ')
+        print(f'{n} x {j} = {n * j}\t ',end='')
--- 鸡兔同笼作业.py
+++ 鸡兔同笼作业.py
@@ -1,6 +1,8 @@
-for c in range(0 , 36):
-    r = 35 - c
-    if 2 * c + 4 * r == 94:
+h = int(input("请输入有多少个头"))
+f = int(input('请输入有多少个脚'))
+for c in range(0 , h):
+    r = h - c
+    if 2 * c + 4 * r == f:
         print('鸡的数量是'+str(c))
         print('兔的数量是'+str(r))
 
