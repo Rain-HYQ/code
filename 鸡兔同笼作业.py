400a74e86be711a9c37162983334a05a56b1a3ae
Rain <xbmb250@hotmail.com>
2024-08-08 10:54:03 +0800

    作业

--- 鸡兔同笼作业.py
+++ 鸡兔同笼作业.py
@@ -3,6 +3,5 @@ for c in range(0 , 36):
     if 2 * c + 4 * r == 94:
         print('鸡的数量是'+str(c))
         print('兔的数量是'+str(r))
-print('hello word！')
 
 
---
+++ 鸡兔同笼另类方法.py
@@ -0,0 +1,7 @@
+c=0
+r = 35 - c
+while 2*c + 4 * r != 94:
+    c += 1
+    r = 35 - c
+print(c)
+print(r)
